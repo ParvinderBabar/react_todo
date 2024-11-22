1be95bf0a9b4b65800e8b6f15cd06c8f8c7a6dd8
parvinder <parvinderbabar@gmail.com>
2024-11-22 15:16:29 -0500

    Complete Lesson 1: Todo List

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-// src/App.jsx
-import TodoList from "./TodoList";
-import AddTodoForm from "./Component/AddTodoForm.jsx";
-const App = () => {
-  return (
+
+import './App.css'
+ const todoList = [
+ { id: 1, title: "Complete assignment APACHE NIFI" },
+  { id: 2, title: "Buy groceries" },
+  { id: 3, title: "Going Gym" }
+];
+function App() {
+return (
     <div>
       <h1>Todo List</h1>
-      <TodoList />
-      {/*  added componet here */}
-      <AddTodoForm />
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
-export default App;
+
+export default App
