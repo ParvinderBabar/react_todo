b02beb5e0b2e106f49f98b333a5128dd8bb78e48
parvinder <parvinderbabar@gmail.com>
2024-11-27 09:38:08 -0500

    lesson 2 completed ,added form

---
+++ src/Component/AddTodoForm.jsx
@@ -0,0 +1,11 @@
+const AddTodoForm = () => {
+  return (
+    <form>
+      <label htmlFor="Title">Todo Task</label>
+       <input type="text" id="todoTitle" />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default AddTodoForm; 
